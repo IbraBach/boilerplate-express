24559fdf59a79933db36f2db5bfdccc5062c8f3b
IbraBach <ibraalberola@gmail.com>
2021-07-04 20:29:41 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,39 +1,29 @@
 var express = require('express');
+var config = require('dotenv').config();
 var app = express();
 
 
 
+var absolutePath = __dirname + "/views/index.html";
+var staticFiles = __dirname + "/public";
 
+app.get("/", function (req, res) {
+  res.sendFile(absolutePath);
+});
 
+app.use("/public",express.static(staticFiles));
 
+app.get("/json", function (req, res) {
+    var body = "Hello json";
 
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        body = "HELLO JSON";
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
+    res.json({
+        "message": body
+    });
+  });
 
  module.exports = app;
