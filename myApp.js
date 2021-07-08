0631affbdc29dde7035beef64856ff8ffe965558
IbraBach <ibraalberola@gmail.com>
2021-07-08 19:56:44 +0200

    Update myApp.js
    "Get Route Parameter Input from the Client" challenge

--- myApp.js
+++ myApp.js
@@ -25,11 +25,15 @@ app.get("/now", function(req, res, next){
   res.send({time: req.time});
 });
 
+app.get("/:word/echo", function (req, res) {
+  res.send({echo: req.params.word});
+});
+
 app.get("/json", function (req, res) {
     var body = "Hello json";
 
     if (process.env.MESSAGE_STYLE === "uppercase"){
-        body = "HELLO JSON";
+      body = "HELLO JSON";
     }
 
     res.json({
